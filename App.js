45d316ea3bd08febe205a2b647c4c39592dfbe6a
Stella <wlsl5353@nate.com>
2022-09-02 17:46:45 +0900

    weather

--- App.js
+++ App.js
@@ -1,20 +1,143 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Location from "expo-location";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  Dimensions,
+  ActivityIndicator,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import { Fontisto } from "@expo/vector-icons";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
+const API_KEY = "1e5a806c698054ac9377080a58bbb09c";
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false },
+    );
+
+    setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`,
+    );
+    const json = await response.json();
+    console.log(json);
+    setDays(json.daily);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+        <Fontisto name="cloudy" size={68} color="white" />
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled // 자연스럽게 페이지 넘김처리
+        showsHorizontalScrollIndicator={false} // 하단에 보이는 스크롤 숨김
+        contentContainerStyle={styles.weather} // scrollview에서 먹히는 prop
+      >
+        {days.length === 0 ? (
+          <View style={{ ...styles.day, alignItems: "center" }}>
+            <ActivityIndicator
+              color="white"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  width: "100%",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Fontisto
+                  name={icons[day.weather[0].main]}
+                  size={24}
+                  color="white"
+                />
+              </View>
+              <Text style={styles.temp}>{day.temp.day}</Text>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text styles={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
+      </ScrollView>
     </View>
   );
 }
 
+// StyleSheet를 쓰지않아도 되지만 자동완성 기능을 제공해줌.
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1.2,
+    backgroundColor: "blue",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {
+    backgroundColor: "green",
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    backgroundColor: "teal",
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 20,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -20,
+    fontSize: 60,
+  },
+  tinyText: {
+    fontSize: 20,
   },
 });
